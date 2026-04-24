009a27798ea3d4b284ef45d336a6ccb62279ca79
Marvin Grunwald <marvin.grunwald@student.htw-berlin.de>
2026-04-24 14:58:04 +0200

    Initial commit

---
+++ packages/language/src/index.ts
@@ -0,0 +1,5 @@
+export * from './doc-lang-module.js';
+export * from './doc-lang-validator.js';
+export * from './generated/ast.js';
+export * from './generated/grammar.js';
+export * from './generated/module.js';
