23714bb51070be7e7a61154255df85b811f693a6
Marvin Grunwald <marvin.grunwald@student.htw-berlin.de>
2026-04-27 21:16:30 +0200

    feat: add first validators
    - Fix return type grammar to work with validator
    - Add Naming convention validators
    - Add line break checks for descriptions/section headers

--- packages/language/src/doc-lang-validator.ts
+++ packages/language/src/doc-lang-validator.ts
@@ -1,5 +1,5 @@
-import type { /* ValidationAcceptor,*/ ValidationChecks } from 'langium';
-import type { DocLangAstType } from './generated/ast.js';
+import type { ValidationAcceptor, ValidationChecks } from 'langium';
+import { Description, Entity, Field, Func, Sect, type DocLangAstType } from './generated/ast.js';
 import type { DocLangServices } from './doc-lang-module.js';
 
 /**
@@ -9,7 +9,11 @@ export function registerValidationChecks(services: DocLangServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.DocLangValidator;
     const checks: ValidationChecks<DocLangAstType> = {
-        // Person: validator.checkPersonStartsWithCapital
+        Sect: validator.checkSectionNamingConvention,
+        Func: validator.checkFunctionHasReturnType,
+        Entity: validator.checkEntityNamingConvention,
+        Description: validator.checkDescriptionOnlyOneLine,
+        Field: validator.checkFieldNamingConvention
     };
     registry.register(checks, validator);
 }
@@ -19,13 +23,48 @@ export function registerValidationChecks(services: DocLangServices) {
  */
 export class DocLangValidator {
 
-    // checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
-    //     if (person.name) {
-    //         const firstChar = person.name.substring(0, 1);
-    //         if (firstChar.toUpperCase() !== firstChar) {
-    //             accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
-    //         }
-    //     }
-    // }
+    // Error checks
+    checkSectionNamingConvention(section: Sect, accept: ValidationAcceptor): void {
+        if (section.text && !this._checkOneLineString(section.text)) {
+            accept('error', 'Section text can be only one line.', { node: section, property: 'text' });
+        }
+    }
 
+    // Warning
+    checkDescriptionOnlyOneLine(description: Description, accept: ValidationAcceptor): void {
+        if (!this._checkOneLineString(description.text)) {
+            accept('warning', 'Description should be only one line.', { node: description });
+        }
+    }
+
+    checkFunctionHasReturnType(func: Func, accept: ValidationAcceptor): void {
+        if (!func.returnType) {
+            accept('warning', `Function should have a return type. ${func.returnType}`, { node: func, property: 'returnType' });
+        }
+    }
+
+    // Info
+    checkEntityNamingConvention(entity: Entity, accept: ValidationAcceptor): void {
+        if (entity.name && !this._checkCamelCaseString(entity.name)) {
+            accept('info', 'Entity name should be in upper camelCase.', { node: entity, property: 'name' });
+        }
+    }
+
+    checkFieldNamingConvention(field: Field, accept: ValidationAcceptor): void {
+        if (field.name && !this._checkCamelCaseString(field.name, true)) {
+            accept('info', 'Field name should be in lowerCamelCase.', { node: field, property: 'name' });
+        }
+    }
+
+    // Helper methods for validation checks
+    _checkCamelCaseString(value: string, isLowerCamelCase: boolean = false): boolean {
+        const camelCasePattern = isLowerCamelCase ? /^[a-z]+([A-Z][a-z]*)*$/ : /^[A-Z][a-zA-Z]*$/;
+        return camelCasePattern.test(value);
+    }
+
+    _checkOneLineString(value: string): boolean {
+        return !value.includes('\n');
+    }
 }
+
+
