29a08cbb11cd84f3ab932dcd3d7cc9b86c59ac48
Marvin Grunwald <marvin.grunwald@student.htw-berlin.de>
2026-06-04 10:39:23 +0200

    feat: load example files instead of hard coding

--- packages/web/src/dsleditor/languagespec.ts
+++ packages/web/src/dsleditor/languagespec.ts
@@ -1,5 +1,4 @@
-
-
+import exampleCode from '../../../../examples/first-example.dlang?raw';
 
 export const languageId = 'doc-lang';
 export const dslExtension = '.dlang';
@@ -7,45 +6,6 @@ export const dslExtension = '.dlang';
 export const dslpublisher = 'Grunwald';
 export const dslversion = '0.0.1';
 
-export const exampleCode = `
-Proj "Example Project"
-Descr "Here will be the documentation for the 'Example Project'. "
-
-Sect "Example Section"
-
-Obj Logger{ 
-    "Core logging component of the system."
-    
-    name: string
-    id: int = 0
-    log = Log
-
-    \`\`\`
-    Logger logger = new Logger()
-    \`\`\`
-} 
-
-Obj Backend{
-    "Main backend container object."
-
-    logger: Logger
-    
-    \`\`\`
-    Backend backend = new Backend()
-    backend.logger.log("This is a log message.")
-    \`\`\`
-} 
-
-Func Log{ 
-    "Handles logging of messages."
-
-    message: string
-    return: void
-
-    \`\`\`
-    logger.Log("This is a log message.")
-    \`\`\`
-}
-`;
+export { exampleCode };
 
 export const codeUri = '/workspace/doc.dlang';
--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -2,6 +2,9 @@ import MarkdownIt from "markdown-it";
 import markdownItAttrs from "markdown-it-attrs";
 import mermaid from "mermaid";
 
+import dummyMD from '../../../examples/output/first-example.md?raw';
+
+
 import {
   startMonacoEditor,
   getCurrentCode,
@@ -98,128 +101,3 @@ export const runDsl = async () => {
     console.error(e);
   }
 };
-
-const dummyMD = `
-# Wiki Page for "Example Project"
-
-Here will be the documentation for the "Example Project".
-
-## Table of Contents
-
-- [Diagrams](#00)
-  - [Class Diagram](#00-01)
-  - [Dependency Diagram](#00-02)
-- [Example Section](#01)
-
----
-
-## Diagrams {#00}
-
-### Class Diagram {#00-01}
-
-\`\`\`mermaid
-classDiagram
-direction LR
-
-class Backend {
-  +Logger logger
-}
-
-class Logger {
-  +string name
-  +int id = 0
-  +Func Log
-}
-
-class Log {
-  +void Log(string message)
-}
-
-Backend --> Logger : contains
-Logger --> Log : uses
-\`\`\`
-
-## Dependency Diagram {#00-02}
-
-\`\`\`mermaid
-graph LR
-
-Backend["Backend"]
-Logger["Logger"]
-Log["Log Function"]
-
-Backend -->|"depends on"| Logger
-Logger -->|"depends on"| Log
-\`\`\`
-
----
-
-## Example Section {#01}
-
-- [Objects](#01-01)
-   - [Logger](#01-01-01)
-   - [Backend](#01-01-02)
-- [Functions](#01-02)
-   - [Log](#01-02-01)
-
----
-
-### Objects {#01-01}
-
-#### \`Logger\` {#01-01-01}
-
-Core logging component of the system.
-
-**Fields**
-
-- **name**: \`string\`
-- **id**: \`int\` = 0
-- **Log**: \`Func\` = [\`Log\`](#01-02-01)
-
-**Usage**
-\`\`\`
-Logger logger = new Logger();
-\`\`\`
-
-**See also**
-[\`Log\`](#01-02-01) [\`Backend\`](#01-01-02)
-
----
-
-#### \`Backend\` {#01-01-02}
-
-Main backend container object.
-
-**Fields**
-
-- **logger**: [\`Logger\`](#01-01-01) = new Logger()
-
-**Usage**
-\`\`\`
-Backend backend = new Backend();
-backend.logger.log("This is a log message.");
-\`\`\`
-
-**See also**
-[\`Logger\`](#01-01-01)
-
----
-### Functions {#01-02}
-
-#### \`Log\` {#01-02-01}
-
-Handles logging of messages.
-
-**Parameters**
-- **message**: \`string\`
-
-**Returns**: \`void\`
-
-**Usage**
-\`\`\`
-logger.Log("This is a log message.");
-\`\`\`
-
-**See also**
-[\`Logger\`](#01-01-01)
-`;
